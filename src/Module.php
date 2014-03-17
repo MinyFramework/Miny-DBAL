fedae7cb903a546016270f09095edc83f104b364
Dániel Buga <bugadani@gmail.com>
2014-03-17 22:29:03 +0100

    Add module and dependencies.

---
+++ src/Module.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the Miny framework.
+ * (c) Dániel Buga <daniel@bugadani.hu>
+ *
+ * For licensing information see the LICENSE file.
+ */
+
+namespace Modules\DBAL;
+
+use Miny\Application\BaseApplication;
+
+class Module extends \Miny\Modules\Module
+{
+
+    public function init(BaseApplication $app)
+    {
+
+    }
+}
